30b92389dce005e3cb8ae4de7104b241644204a5
Salmen BOUHAMED <salmen@gmx.us>
2022-09-08 22:24:27 +0200

    feat(Menu Bar): fix on scroll

--- src/components/menu-bar/menu-bar.tsx
+++ src/components/menu-bar/menu-bar.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import "./menu-bar.scss"
 
 function MenuBar() {
+    useEffect(() => {
+        window.addEventListener('scroll', isSticky);
+        return () => {
+            window.removeEventListener('scroll', isSticky);
+        };
+    });
+
+    const isSticky = (event: any) => {
+        const header: any = document.querySelector('.MenuBar');
+        const scrollTop = window.scrollY;
+        scrollTop > 0 ? header.classList.remove('unfixed') : header.classList.add('unfixed')
+    };
+
     return (
-        <nav className="MenuBar">
+        <nav className="MenuBar unfixed">
             <div className="nav-bar">
                 <a className="menuLinkLogo">
                     <img src="https://hex.com/_assets/img/wordmark.svg" alt="Hexy Logo svg" />
